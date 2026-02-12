986dd0a213f21727cef33ed5b2b0ddd222a55464
Jisse Reitsma <jisse@yireo.com>
2026-02-12 11:57:58 +0100

    Add trigger name arg for `db:trigger:remove` CLI

--- Console/Command/RemoveTrigger.php
+++ Console/Command/RemoveTrigger.php
@@ -3,6 +3,7 @@
 namespace Yireo\ResetMysqlTriggers\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -21,6 +22,7 @@ class RemoveTrigger extends Command
     {
         $this->setName('db:trigger:remove');
         $this->setDescription('Reset triggers');
+        $this->addArgument('trigger', InputArgument::OPTIONAL , 'Trigger name');
         $this->addOption('all', 'a', InputOption::VALUE_NONE, 'Remove all triggers');
         parent::configure();
     }
@@ -28,12 +30,28 @@ class RemoveTrigger extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $all = $input->getOption('all');
-        foreach ($this->triggerQuery->getTriggers() as $trigger) {
-            if (false === $all) {
-                continue;
-            }
+        $triggerName = $input->getArgument('trigger');
 
-            $this->triggerQuery->removeTrigger($trigger['TRIGGER_NAME']);
+        if (false === $all && empty($triggerName)) {
+            $output->writeln('<error>Specify either a trigger name or use the option --all</error>');
+            return Command::FAILURE;
+        }
+
+        $triggers = $this->triggerQuery->getTriggers();
+        if ($all) {
+            foreach ($triggers as $trigger) {
+                $this->triggerQuery->removeTrigger($trigger['TRIGGER_NAME']);
+            }
+        }
+
+        if (!empty($triggerName)) {
+            foreach ($triggers as $trigger) {
+                if ($trigger['TRIGGER_NAME'] !== $triggerName) {
+                    continue;
+                }
+
+                $this->triggerQuery->removeTrigger($trigger['TRIGGER_NAME']);
+            }
         }
 
         return Command::SUCCESS;
